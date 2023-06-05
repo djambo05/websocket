55f2e4a5894dbb84d85d374e9a16e75ec18d14f1
Djamalutin <fisher.gerryy@mail.ru>
2023-06-05 22:14:53 +0300

    fix: refacrot

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import "./App.css";
-import { BinanceService } from "./services/BinanceApi.service";
 import { Binance } from "./components/Binance";
 
 function App() {
-  BinanceService.getAllPrices();
-  BinanceService.getOnePrice("BTCUSDT");
   return (
-    <div className="App">
+    <div>
       <Binance />
     </div>
   );
